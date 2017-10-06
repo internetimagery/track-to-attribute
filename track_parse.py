b99abe516ae56ac30d3b2525c2b38177baef1899
Jason Dixon <jason.dixon.email@gmail.com>
2017-10-07 01:07:40 +1300

    some tidy comments

--- track_parse.py
+++ track_parse.py
@@ -6,12 +6,13 @@ import re
 def run(file_path):
     """ Pull out tracker data and pass it to calling process """
 
+    # Bring in file
     import nuke
     nuke.scriptClear()
     nuke.scriptOpen(file_path)
 
+    # Parse tracker data
     trackers = {}
-
     for node in (n["tracks"].toScript() for n in nuke.allNodes() if n.Class() == "Tracker4"):
         for track in re.finditer(r"\"([^\"])\"\s*{\s*curve\s*x\d+\s*([\d\s\.\-xe])}\s*{\s*curve\s*x\d+\s*([\d\s\.\-xe])}", node):
             name = track.group(1)
@@ -19,6 +20,7 @@ def run(file_path):
             y = (a for a in track.group(3).split(" ") if a[0] != "x")
             trackers[name] = (x, y)
 
+    # Send tracker data back to calling process
     print(json.dumps(trackers))
     return True
 
