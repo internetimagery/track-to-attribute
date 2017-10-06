0de25cd29c1035812b1020838dc51ca8d628b128
Jason Dixon <jason.dixon.email@gmail.com>
2017-10-07 01:05:31 +1300

    nuke logic

--- logic.py
+++ logic.py
@@ -1,17 +1,67 @@
 # Make things work!
+import maya.cmds as cmds
+import subprocess
+import os.path
+import json
 
-def get_tracker(file_path):
+def get_tracker(file_path, nuke_path="nuke"):
     """ Get tracker data from nuke file """
-    pass
+
+    # Validate file and build command
+    if not os.path.isfile(file_path):
+        raise RuntimeError("File does not exist: {}".format(file_path))
+    command = "{} -t -- '{}' '{}'".format(
+        nuke_path,
+        os.path.join(os.path.dirname(__file__), "track_parse.py"),
+        file_path)
+
+    # Run nuke and collect data.
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    data = {}
+    while True:
+        line = process.readline()
+        if not line:
+            break
+        try:
+            data = json.loads(line)
+        except ValueError:
+            pass
+    return data
 
 def get_attribute():
     """ Get selected attribute from channelbox """
-    pass
+    for obj, attr in zip(cmds.ls(sl=True) or [], cmds.channelBox("mainChannelBox", sma=True, q=True) or []):
+        if cmds.attributeQuery(attr, n=obj, ex=True):
+            return "{}.{}".format(obj, attr)
+    return ""
 
 def set_keys(attr, startF, keys):
     """ Set keys on attribute """
-    pass
+    for i, key in enumerate(keys):
+        frame = i + startF
+        cmds.setKeyframe(attr, t=frame, v=key)
 
-def apply_data(tracker, stabalize, data, attrX, attrY):
+def apply_data(tracker, stabalize, data, attrX, attrY, scaleX, scaleY):
     """ Take data. Apply it to attributes. """
-    pass
+
+    # Get data
+    tracker_data = data[tracker]
+    stabalize_data = data[stabalize] if stabalize else []
+
+    # Calculate data
+    dataX = []
+    dataY = []
+    for i, (x, y) in enumerate(zip(*tracker_data)):
+        try:
+            dataX.append((x - stabalize_data[0][i]) * scaleX)
+            dataY.append((y - stabalize_data[1][i]) * scaleY)
+        except IndexError:
+            dataX.append(x * scaleX)
+            dataY.append(x * scaleY)
+
+    # Apply data
+    start = cmds.currentTime(q=True)
+    if attrX:
+        set_keys(attrX, start, dataX)
+    if attrY:
+        set_keys(attrY, start, dataY)
--- track_parse.py
+++ track_parse.py
@@ -1,8 +1,26 @@
 # Parse out tracker data.
+from __future__ import print_function
+import re
 
 def run(file_path):
     """ Pull out tracker data and pass it to calling process """
-    pass
+
+    import nuke
+    nuke.scriptClear()
+    nuke.scriptOpen(file_path)
+
+    trackers = {}
+
+    for node in (n["tracks"].toScript() for n in nuke.allNodes() if n.Class() == "Tracker4"):
+        for track in re.finditer(r"\"([^\"])\"\s*{\s*curve\s*x\d+\s*([\d\s\.\-xe])}\s*{\s*curve\s*x\d+\s*([\d\s\.\-xe])}", node):
+            name = track.group(1)
+            X = (a for a in track.group(2).split(" ") if a[0] != "x")
+            y = (a for a in track.group(3).split(" ") if a[0] != "x")
+            trackers[name] = (x, y)
+
+    print(json.dumps(trackers))
+    return True
 
 if __name__ == '__main__':
-    pass
+    import sys
+    sys.exit(run(sys.argv[1]) is False)
