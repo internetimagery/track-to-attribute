a4b2d79826c9c29e3cd081e5edf989280c856d34
Jason Dixon <jdixon@wetafx.co.nz>
2017-10-12 10:35:49 +1300

    eems to work

--- test_parse.py
+++ test_parse.py
@@ -6,27 +6,20 @@ import xml.etree.ElementTree as ET
 def parse(file_):
     with open(file_, "r") as f:
         # for node in ET.fromstring(f.read()).find("./Project/NodesCollection") or []:
+        result = {}
         for node in ET.fromstring(f.read()).findall("./Project/NodesCollection/item/TrackerContext/Item"):
-            print node
-            print node.find("Label").text
-
-            # for tracker in node.find("TrackerContext") or []:
-            #     try:
-            #         name = tracker.find("Label").text
-            #
-            #         for n in tracker:
-            #             try:
-            #                 if n.find("Name").text == "centerPoint":
-            #                     for p in n:
-            #                         print(p)
-            #                     # Perhaps keyframes reside here for tracker motion?
-            #             except:
-            #                 pass
-            #     except AttributeError:
-            #         pass
+            name = node.find("Label").text
+            curves = []
+            for curve in node.findall("Item/[Name='centerPoint']/item/Curve/KeyFrameSet"):
+                keys = {}
+                for key in curve.findall("item"):
+                    keys[float(key.find("Time").text)] = float(key.find("Value").text)
+                curves.append(keys)
+            result[name] = curves
+        return result
 
 
 
 if __name__ == '__main__':
     from pprint import pprint
-    parse(sys.argv[1])
+    print parse(sys.argv[1])
