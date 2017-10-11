9d6c624245a540e23309a2937de7e3c388f7d419
Jason Dixon <jason.dixon.email@gmail.com>
2017-10-11 21:54:20 +1300

    specify nuke file

--- logic.py
+++ logic.py
@@ -11,7 +11,7 @@ except ImportError:
     import pickle
 
 def parse_frames(curve):
-    """ Parse out keyframes from data """
+    """ Parse out keyframes from nuke curve data """
     result = {}
     frame = 0
     for num in curve.split(" "):
@@ -22,21 +22,27 @@ def parse_frames(curve):
             frame += 1
     return result
 
+def parse_nuke(file_):
+    """ Parse out data from nuke """
+    result = {}
+    nodes = re.compile(r"Tracker4\s{.+?ypos[-\d\n\s]+}", re.DOTALL)
+    trackers = re.compile(r"\"((?:\\.|[^\"\\])*)\"\s+{curve\s+([-\d\.\sex]+)}\s+{curve\s+([-\d\.\sex]+)}") # :: "tracker" {curve x12 34.56 67.54-e32 43.4554}
+    for node in nodes.finditer(file_):
+        for tracker in trackers.finditer(node.group(0)):
+            result[tracker.group(1)] = (
+                parse_frames(tracker.group(2)),
+                parse_frames(tracker.group(3))
+                )
+    return result
+
 def get_tracks(file_path):
     """ Get Tracker data. """
     if not os.path.isfile(file_path):
         raise RuntimeError("File does not exist: {}".format(file_path))
     with open(file_path, "r") as f:
-        result = {}
-        nodes = re.compile(r"Tracker4\s{.+?ypos[-\d\n\s]+}", re.DOTALL)
-        trackers = re.compile(r"\"((?:\\.|[^\"\\])*)\"\s+{curve\s+([-\d\.\sex]+)}\s+{curve\s+([-\d\.\sex]+)}") # :: "tracker" {curve x12 34.56 67.54-e32 43.4554}
-        for node in nodes.finditer(f.read()):
-            for tracker in trackers.finditer(node.group(0)):
-                result[tracker.group(1)] = (
-                    parse_frames(tracker.group(2)),
-                    parse_frames(tracker.group(3))
-                    )
-        return result
+        if file_path[-3:] == ".nk":
+            return parse_nuke(f.read())
+        return {}
 
 def get_attribute():
     """ Get selected attribute from channelbox """
