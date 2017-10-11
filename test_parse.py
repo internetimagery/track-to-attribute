a4b4c6bc762eae3b8f00d887ec2eaf95382723e8
Jason Dixon <jason.dixon.email@gmail.com>
2017-10-11 23:17:25 +1300

    test

--- test_parse.py
+++ test_parse.py
@@ -5,8 +5,23 @@ import xml.etree.ElementTree as ET
 
 def parse(file_):
     with open(file_, "r") as f:
-        root = ET.fromstring(f.read())
-        print(root.findall("TrackerContext"))
+        for node in ET.fromstring(f.read()).find("./Project/NodesCollection") or []:
+            for tracker in node.find("TrackerContext") or []:
+                try:
+                    name = tracker.find("Label").text
+
+                    for n in tracker:
+                        try:
+                            if n.find("Name").text == "centerPoint":
+                                for p in n:
+                                    print(p)
+                                # Perhaps keyframes reside here for tracker motion?
+                        except:
+                            pass
+                except AttributeError:
+                    pass
+
+
 
 if __name__ == '__main__':
     from pprint import pprint
