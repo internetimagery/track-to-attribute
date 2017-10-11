d9c8d7682d3079c49febf991e6f055de8d836570
Jason Dixon <jdixon@wetafx.co.nz>
2017-10-12 09:38:35 +1300

    huge xpath

--- test_parse.py
+++ test_parse.py
@@ -5,21 +5,25 @@ import xml.etree.ElementTree as ET
 
 def parse(file_):
     with open(file_, "r") as f:
-        for node in ET.fromstring(f.read()).find("./Project/NodesCollection") or []:
-            for tracker in node.find("TrackerContext") or []:
-                try:
-                    name = tracker.find("Label").text
+        # for node in ET.fromstring(f.read()).find("./Project/NodesCollection") or []:
+        for node in ET.fromstring(f.read()).findall("./Project/NodesCollection/item/TrackerContext/Item"):
+            print node
+            print node.find("Label").text
 
-                    for n in tracker:
-                        try:
-                            if n.find("Name").text == "centerPoint":
-                                for p in n:
-                                    print(p)
-                                # Perhaps keyframes reside here for tracker motion?
-                        except:
-                            pass
-                except AttributeError:
-                    pass
+            # for tracker in node.find("TrackerContext") or []:
+            #     try:
+            #         name = tracker.find("Label").text
+            #
+            #         for n in tracker:
+            #             try:
+            #                 if n.find("Name").text == "centerPoint":
+            #                     for p in n:
+            #                         print(p)
+            #                     # Perhaps keyframes reside here for tracker motion?
+            #             except:
+            #                 pass
+            #     except AttributeError:
+            #         pass
 
 
 
