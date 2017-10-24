8fae09a0f99e06ddc59f77cc0b288dab1af26fad
Jason Dixon <jdixon@wetafx.co.nz>
2017-10-24 16:51:27 +1300

    more fleshing out

--- redesign.py
+++ redesign.py
@@ -9,23 +9,28 @@
 # entry: [x/y/angle] [track] [stabalize/angle] [remove]
 
 import maya.cmds as cmds
+import logic
 
 NONE = "---"
 
 
 def get_attribute():
     """ Get selected attribute from channelbox """
-    for obj in cmds.ls(sl=True) or []:
-        for attr in cmds.channelBox("mainChannelBox", sma=True, q=True) or []:
-            if cmds.attributeQuery(attr, n=obj, ex=True):
-                return "{}.{}".format(obj, attr)
-    return ""
+    return set(["{}.{}".format(o, at) for o in cmds.ls(sl=True) for at in cmds.channelBox("mainChannelBox", sma=True, q=True) or [] if cmds.attributeQuery(at, n=o, ex=True)])
 
+def browse():
+    """ Open file browser """
+    path = cmds.fileDialog2(
+        fm=1,
+        ff="Tracker file (*.nk *.ntp)",
+        dir=logic.real_path(cmds.textFieldButtonGrp(s.nuke, q=True, tx=True)))
+    if path:
+        s.load_tracker(path[0])
 
 class Attribute(object):
     """ Attribute gui entry """
-    def __init__(s, parent, attr="", trackers=[]):
-        s.active = True
+    def __init__(s, parent, attr, trackers):
+        s._active = True
         s._attr = cmds.textField(w=400, tx=attr, p=parent)
         s._axis = cmds.optionMenu(p=parent)
         for t in ["X", "Y", "Angle"]:
@@ -36,12 +41,17 @@ class Attribute(object):
         s._track2 = cmds.optionMenu(p=parent)
         for t in trackers:
             cmds.menuItem(l=t, p=s._track2)
+        s._key = cmds.button(l="Key", c=s.delete, bgc=(0.2, 0.5, 0.4), p=parent)
         s._del = cmds.button(l="Remove", c=s.delete, bgc=(0.4, 0.3, 0.3), p=parent)
+    def key(s, func=None):
+        """ Key attr data """
+        if s._active:
+            return (s.attr, s.axis, s.track1, s.track2)
     def delete(s, *_):
         """ Remove UI element """
-        if s.active:
-            cmds.deleteUI([s._attr, s._axis, s._track1, s._track2, s._del])
-            s.active = False
+        if s._active:
+            cmds.deleteUI([s._attr, s._axis, s._track1, s._track2, s._key, s._del])
+            s._active = False
     attr = property(
         (lambda s:cmds.textField(s._attr, q=True, tx=True)),
         (lambda s, x:cmds.textField(s._attr, e=True, tx=x)))
@@ -52,24 +62,34 @@ class Attribute(object):
 class Window(object):
     def __init__(s):
         """ Apply tracker data to attributes """
-        s.trackers = [NONE, "one", "two", "three!"]
-        s.attributes = []
-        win = cmds.window(rtf=True)
-        main = cmds.columnLayout(adj=True)
-        cmds.button(l="Add Attribute", c=s.add_attr)
-        cmds.scrollLayout(cr=True, h=300)
-        s.root = cmds.rowColumnLayout(nc=5)
-        cmds.text(l="Attribute")
-        cmds.text(l="Axis")
-        cmds.text(l="Tracker")
-        cmds.text(l="Stabalize / Angle")
-        cmds.button(l="Clear all", c=s.clear_all)
-        for i in range(3):
-            s.attributes.append(Attribute(s.root, "Attr! %s" % i, s.trackers))
-        cmds.setParent(main)
-        cmds.separator()
-        cmds.button(l="Keyframe")
-        cmds.showWindow()
+        # Get tracker file
+        path = cmds.fileDialog2(fm=1, ff="Tracker file (*.nk *.ntp)")
+        if path:
+            path = path[0]
+
+            # Load trackers
+            s.data = logic.get_tracks(path)
+            if not s.data:
+                raise RuntimeError("No tracking data found.")
+
+            # Prep data
+            s.trackers = [NONE] + s.data.keys()
+            s.attributes = []
+
+            # Open window
+            win = cmds.window(t=path)
+            main = cmds.columnLayout(adj=True)
+            cmds.button(l="Add from Channel Box", bgc=(0.2, 0.5, 0.4), c=s.add_attr)
+            s.root = cmds.rowColumnLayout(nc=6)
+            cmds.text(l="Attribute")
+            cmds.text(l="Axis")
+            cmds.text(l="Tracker")
+            cmds.text(l="Stabalize / Angle")
+            cmds.button(l="Key All")
+            cmds.button(l="Clear all", c=s.clear_all)
+            for i in range(3):
+                s.attributes.append(Attribute(s.root, "Attr! %s" % i, s.trackers))
+            cmds.showWindow()
 
     def clear_all(s, *_):
         """ Remove all attrs """
@@ -77,6 +97,12 @@ class Window(object):
             at.delete()
         s.attributes = []
 
+    def key_all(s, *_):
+        """ Key all """
+        result = [(at.attr, at.axis, at.track1, at.track2)for at in s.attributes]
+
     def add_attr(s, *_):
         """ Add new attribute from channelbox """
-        s.attributes.append(Attribute(s.root, get_attribute(), s.trackers))
+        attrs = get_attribute()
+        for at in attrs:
+            s.attributes.append(Attribute(s.root, at, s.trackers))
