212243b72a48a77803d86382f1198f3a5ffe2e6b
Jason Dixon <jason.dixon.email@gmail.com>
2017-10-24 22:26:58 +1300

    validation and styles

--- redesign.py
+++ redesign.py
@@ -13,7 +13,12 @@ import maya.mel as mel
 import logic
 
 NONE = "---"
-
+BLACK = (0.1,0.1,0.1)
+GREEN = (0.2, 0.5, 0.4)
+RED = (0.4, 0.3, 0.3)
+L_GREEN = [a * 1.5 for a in GREEN]
+D_GREEN = [a * 0.4 for a in GREEN]
+D_RED = [a * 0.4 for a in RED]
 
 def get_attribute():
     """ Get selected attribute from channelbox """
@@ -28,6 +33,14 @@ def browse():
     if path:
         s.load_tracker(path[0])
 
+def validate_attribute(attr):
+    """ Check attribute is valid and exists """
+    try:
+        obj, at = attr.split(".")
+        return cmds.attributeQuery(at, n=obj, k=True)
+    except (ValueError, RuntimeError):
+        return False
+
 class Helper(object):
     def __init__(s, data):
         """ Helper GUI to collect range information. {attr: Keyset()} """
@@ -38,20 +51,32 @@ class Helper(object):
             s.state.append({ "attr": attr, "time": s.data[attr].max[0]})
         s.state_pos = 0
 
+        name = "KeyMatch"
+        if cmds.window(name, q=True, ex=True):
+            cmds.deleteUI(name)
         s.win = cmds.window(t="Key Match")
         cmds.columnLayout(adj=True)
         cmds.text(l="Please position attribute:")
         s.text = cmds.text(l="ATTR", hl=True)
         cmds.separator()
-        s.capt = cmds.button(l="Capture Attribute", c=s.capture)
+        s.capt = cmds.button(l="Capture Attribute", bgc=GREEN, h=100, c=s.capture)
         cmds.showWindow()
         s.refresh()
+
+    def lock_time(s):
+        """ Keep us locked in time position """
+        if cmds.window(s.win, q=True, ex=True):
+            if cmds.currentTime(q=True) != s.time:
+                cmds.currentTime(s.time)
+            cmds.scriptJob(e=("timeChanged", s.lock_time), ro=True)
 
     def refresh(s):
         """ Set gui to capture frame """
         attr = s.state[s.state_pos]["attr"]
         time = s.state[s.state_pos]["time"]
         cmds.text(s.text, e=True, l="<h1>{} : {}</h1>".format(attr.split(".")[0], cmds.attributeName(attr, n=True)))
+        s.time = time
+        s.lock_time()
         cmds.currentTime(time)
 
     def capture(s, *_):
@@ -87,9 +112,9 @@ class Helper(object):
 class Attribute(object):
     """ Attribute gui entry """
     def __init__(s, parent, attr, trackers, callback):
-        s._active = True
+        s.active = True
         s._callback = callback
-        s._attr = cmds.textField(w=400, tx=attr, p=parent)
+        s._attr = cmds.textFieldGrp(w=400, tx=attr, bgc=BLACK, tcc=s.validate_attr, p=parent)
         s._axis = cmds.optionMenu(p=parent)
         for t in logic.AXIS:
             cmds.menuItem(l=t, p=s._axis)
@@ -99,20 +124,25 @@ class Attribute(object):
         s._track2 = cmds.optionMenu(p=parent)
         for t in trackers:
             cmds.menuItem(l=t, p=s._track2)
-        s._key = cmds.button(l="Key", c=s.key, bgc=(0.2, 0.5, 0.4), p=parent)
-        s._del = cmds.button(l="Remove", c=s.delete, bgc=(0.4, 0.3, 0.3), p=parent)
+        s._key = cmds.button(l="Key", c=s.key, bgc=GREEN, p=parent)
+        s._del = cmds.button(l="Remove", c=s.delete, bgc=RED, p=parent)
+    def validate_attr(s, text):
+        """ Validate Attribute """
+        if validate_attribute(text):
+            return cmds.textFieldGrp(s._attr, e=True, bgc=D_GREEN)
+        cmds.textFieldGrp(s._attr, e=True, bgc=D_RED)
     def key(s, *_):
         """ Key attr data """
-        if s._active:
+        if s.active:
             s._callback([[s.attr, s.axis, s.track1, s.track2]])
     def delete(s, *_):
         """ Remove UI element """
-        if s._active:
+        if s.active:
             cmds.deleteUI([s._attr, s._axis, s._track1, s._track2, s._key, s._del])
-            s._active = False
+            s.active = False
     attr = property(
-        (lambda s:cmds.textField(s._attr, q=True, tx=True)),
-        (lambda s, x:cmds.textField(s._attr, e=True, tx=x)))
+        (lambda s:cmds.textFieldGrp(s._attr, q=True, tx=True)),
+        (lambda s, x:cmds.textFieldGrp(s._attr, e=True, tx=x)))
     axis = property(lambda s:cmds.optionMenu(s._axis, q=True, v=True))
     track1 = property(lambda s:cmds.optionMenu(s._track1, q=True, v=True))
     track2 = property(lambda s:cmds.optionMenu(s._track2, q=True, v=True))
@@ -135,16 +165,16 @@ class Window(object):
             s.attributes = []
 
             # Open window
-            win = cmds.window(t=path)
+            win = cmds.window(t="Tracker: {}".format(path))
             main = cmds.columnLayout(adj=True)
-            cmds.button(l="Add from Channel Box", bgc=(0.2, 0.5, 0.4), c=s.add_attr)
+            cmds.button(l="Add from Channel Box", bgc=GREEN, c=s.add_attr)
             s.root = cmds.rowColumnLayout(nc=6)
             cmds.text(l="Attribute")
             cmds.text(l="Axis")
             cmds.text(l="Tracker")
             cmds.text(l="Stabalize / Angle")
-            cmds.button(l="Key All")
-            cmds.button(l="Clear all", c=s.clear_all)
+            cmds.button(l="Key All", bgc=GREEN, c=s.key_all)
+            cmds.button(l="Clear all", bgc=RED, c=s.clear_all)
             cmds.showWindow()
 
     def clear_all(s, *_):
@@ -155,7 +185,7 @@ class Window(object):
 
     def key_all(s, *_):
         """ Key all """
-        s.set_keys((at.attr, at.axis, at.track1, at.track2) for at in s.attributes)
+        s.set_keys((at.attr, at.axis, at.track1, at.track2) for at in s.attributes if at.active)
 
     def add_attr(s, *_):
         """ Add new attribute from channelbox """
@@ -165,10 +195,6 @@ class Window(object):
 
     def set_keys(s, info):
         """ Finally start the key setting process """
-        print info
-        data = [at for (at, ax, t1, t2) in info]
-        print data
-        return
 
         # Get frame range we're working in
         slider = mel.eval("$tmp = $gPlayBackSlider")
@@ -180,8 +206,8 @@ class Window(object):
 
         # Process data
         data = {
-            at: logic.process_keys(ax, Fstart, Fstop, s.data[t1] if t1 != NONE else [], s.data[t2] if t2 != NONE else [])
-            for (at, ax, t1, t2) in info}
+            at: logic.process_keys(ax, Fstart, Fstop, s.data[t1], s.data[t2] if t2 != NONE else [])
+            for (at, ax, t1, t2) in info if t1 != NONE and validate_attribute(at)}
 
         # Set keyframes
         if data:
